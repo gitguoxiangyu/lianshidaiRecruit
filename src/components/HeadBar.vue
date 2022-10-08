df659345a71590f5266a6bb828a911df06b475ca
gitguoxiangyu <3317755916@qq.com>
2022-10-08 13:57:44 +0800

    add

--- src/components/HeadBar.vue
+++ src/components/HeadBar.vue
@@ -1,13 +1,13 @@
 <template>
-  <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+  <el-menu :default-active="activeIndex" class="el-menu-demo" router="" mode="horizontal" @select="handleSelect" background-color="black" text-color="#fff" active-text-color="#ffd04b">
     <el-row :gutter="0">
       <el-col :span="10"><div class="img"><img src="../assets/logo.jpg" alt="工作室logo"></div></el-col>
       <el-col :span="2"><el-menu-item index="0">Home</el-menu-item></el-col>
-      <el-col :span="2"><el-menu-item index="1">C语言基础</el-menu-item></el-col>
-      <el-col :span="2"><el-menu-item index="2">前端</el-menu-item></el-col>
-      <el-col :span="2"><el-menu-item index="3">后端</el-menu-item></el-col>
-      <el-col :span="2"><el-menu-item index="4">机器学习</el-menu-item></el-col>
-      <el-col :span="2"><el-menu-item index="5">区块链</el-menu-item></el-col>
+      <el-col :span="2"><el-menu-item index="1" route="/cbaisc">C语言基础</el-menu-item></el-col>
+      <el-col :span="2"><el-menu-item index="2" route="/frontdev">前端</el-menu-item></el-col>
+      <el-col :span="2"><el-menu-item index="3" route="/backdev">后端</el-menu-item></el-col>
+      <el-col :span="2"><el-menu-item index="4" route="/machinelearning">机器学习</el-menu-item></el-col>
+      <el-col :span="2"><el-menu-item index="5" route="/blockchain">区块链</el-menu-item></el-col>
       <el-col :span="2"></el-col>
     </el-row>
   </el-menu>
@@ -30,7 +30,10 @@ export default {
 
 <style scoped>
 img{
-  width: 40px;
-  height: 60px;
+  width: 120px;
+}
+
+.el-col{
+  height: 100%;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,31 @@
 import VueRouter from 'vue-router'
+import CBasic from '../components/CBasic.vue'
+import BackDev from '../components/BackDev.vue'
 
 export default new VueRouter({
   routes:[
     {
       path:'/',
     },
+    {
+      path:'/cbaisc',
+      component: CBasic
+    },
+    {
+      path:'/backdev',
+      component: BackDev
+    },
+    {
+      path:'/frontdev',
+      component: BackDev
+    },
+    {
+      path:'/machinelearning',
+      component: BackDev
+    },
+    {
+      path:'/blockchain',
+      component: BackDev
+    }
   ]
 })
